0f9e3097312dd43fe6c4e5bd2f31cb8790418e5f
kevinamstutz <kevin@amstutz.io>
2023-12-07 11:38:53 -0500

    Update `tdmdb` function

--- .config/fish/functions/tdmdb.fish
+++ .config/fish/functions/tdmdb.fish
@@ -5,7 +5,7 @@ function tdmdb
     else
         switch $argv[1]
             case backup
-                cd "$HOME/Box Sync/db_backups/" && pg_dump -h lpvtdmdb01.itap.purdue.edu -U postgres -d tdmdb -Fc -Z 9 > $(date +%Y%m%d%H%M%S)-tdmdb.dump
+                cd "$HOME/Box Sync/db_backups/" && pg_dump -h lpvtdmdb01.itap.purdue.edu -U postgres -d tdmdb -Fc -Z 9 > $(date +%Y%m%d%H%M%S)-tdmdb.dump && ls -lrth | tail -n 1 && cd -
                 return
             case "*";
                 # Any other arguments passed
