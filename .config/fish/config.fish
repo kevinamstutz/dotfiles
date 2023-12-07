781be258b4ec36b9e0e2ced4626e37c1df6ff2e6
kevinamstutz <kevin@amstutz.io>
2023-12-07 11:27:58 -0500

    Add !! and !$ functionality

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,27 @@ set -gx TERM xterm-256color
 alias vim="nvim"
 alias vi="nvim"
 alias lg="lazygit"
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
