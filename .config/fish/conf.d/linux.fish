ddf9cd2f8499b141b64de92d11ec1138b6ae7c26
kevinamstutz <kevin@amstutz.io>
2023-12-07 08:48:59 -0500

    Add $HOME/.ghcup/bin to $PATH

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -8,5 +8,8 @@ case Linux
         if test -e $HOME/.local/Linux/bin/starship
             $HOME/.local/Linux/bin/starship init fish | source
         end
+	if test -e $HOME/.ghcup/bin
+	    fish_add_path $HOME/.ghcup/bin
+	end
     end
 end
